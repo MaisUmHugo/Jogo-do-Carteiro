99fc2563bdb868ef7191f48a5d4335a5419f29b3
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2025-10-25 19:33:54 -0300

    FIX: Arrumado o parallax do fundo

--- Jogo do Carteiro/Assets/Scripts/inGame/Parallax.cs
+++ Jogo do Carteiro/Assets/Scripts/inGame/Parallax.cs
@@ -1,79 +1,21 @@
 using UnityEngine;
 
-public class ParallaxController : MonoBehaviour
+public class Parallax : MonoBehaviour
 {
-    public Transform Cam;
-    Vector3 CamStartPosition;
-    float Distance;
+    Material mat;
+    float distance;
 
-    GameObject[] backgrounds;
-    Material[] mat;
-    float[] backspeed;
-
-    float farthestBack;
-
-    [Range(0.01f, 0.5f)]
-    public float parallaxspeed;
+    [Range(0f, 0.5f)]
+    public float speed = 0.2f;
 
     void Start()
     {
-
-
-        CamStartPosition = Cam.position;
-
-        int backCount = transform.childCount;
-        mat = new Material[backCount];
-        backspeed = new float[backCount];
-
-        backgrounds = new GameObject[backCount];
-
-        for (int i = 0; i < backCount; i++)
-        {
-
-            backgrounds[i] = transform.GetChild(i).gameObject;
-            mat[i] = backgrounds[i].GetComponent<Renderer>().material;
-
-        }
-
-        BackSpeedCalculate(backCount);
-
+        mat = GetComponent<Renderer>().material;
     }
 
-    void BackSpeedCalculate(int backCount)
+    void Update()
     {
-
-        for (int i = 0; i < backCount; i++)
-        {
-
-            if ((backgrounds[i].transform.position.z - Cam.position.z) > farthestBack)
-            {
-
-                farthestBack = backgrounds[i].transform.position.z - Cam.position.z;
-            }
-
-        }
-
-
-        for (int i = 0; i < backCount; i++)
-        {
-
-            backspeed[i] = 1 - (backgrounds[i].transform.position.z - Cam.position.z) / farthestBack;
-
-        }
-
-    }
-
-    private void LateUpdate()
-    {
-        Distance = Cam.position.x - CamStartPosition.x;
-        transform.position = new Vector3(Cam.position.x, transform.position.y, 0);
-
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-
-            float speed = backspeed[i] * parallaxspeed;
-            mat[i].SetTextureOffset("_MainTex", new Vector2(Distance, 0) * speed);
-
-        }
+        distance += Time.deltaTime * speed;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
     }
 }
